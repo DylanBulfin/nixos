a8ad334aa520811a2f3e866fc0d81589cf224a8d
Dylan Bulfin <dylanbulfin@gmail.com>
2024-06-16 09:10:09 -0400

    Removing gnome settings, finish separating home config

--- home.nix
+++ home.nix
@@ -2,7 +2,6 @@
 
 {
   imports = [
-    ./dconf.nix
     ./sway.nix
     ./zsh.nix
   ];
@@ -20,149 +19,12 @@
     swaybg
   ];
 
-  programs.neovim = {
-    enable = true;
-    extraConfig = ''
-      lua << EOF
-          function colemak()
-            vim.keymap.set({ "n", "v" }, "m", "h", { desc = "Left" })
-            vim.keymap.set({ "n", "v", "o" }, "n", "j", { desc = "Down" })
-            vim.keymap.set({ "n", "v", "o" }, "e", "k", { desc = "Up" })
-            vim.keymap.set({ "n", "v" }, "i", "l", { desc = "Right" })
-
-            vim.keymap.set({ "n", "v", "o" }, "j", "e", { desc = "Next end of word" })
-            vim.keymap.set({ "n", "v" }, "k", "n", { desc = "Next match" })
-            vim.keymap.set({ "n", "v" }, "K", "N", { desc = "Previous match" })
-            vim.keymap.set({ "n", "v" }, "u", "i", { desc = "Insert mode" })
-            vim.keymap.set({ "n", "v" }, "U", "I", { desc = "Insert mode at beginning of line" })
-            vim.keymap.set({ "n", "v" }, "l", "u", { desc = "Undo" })
-            vim.keymap.set({ "n", "v" }, "L", "<C-r>", { desc = "Redo" })
-          end
-
-          function qwerty()
-            vim.keymap.set({ "n", "v" }, "h", "h", { desc = "Left" })
-            vim.keymap.set({ "n", "v", "o" }, "j", "j", { desc = "Down" })
-            vim.keymap.set({ "n", "v", "o" }, "k", "k", { desc = "Up" })
-            vim.keymap.set({ "n", "v" }, "l", "l", { desc = "Right" })
-
-            vim.keymap.set({ "n", "v", "o" }, "e", "e", { desc = "Next end of word" })
-            vim.keymap.set({ "n", "v" }, "n", "n", { desc = "Next match" })
-            vim.keymap.set({ "n", "v" }, "N", "N", { desc = "Previous match" })
-            vim.keymap.set({ "n", "v" }, "i", "i", { desc = "Insert mode" })
-            vim.keymap.set({ "n", "v" }, "I", "I", { desc = "Insert mode at beginning of line" })
-            vim.keymap.set({ "n", "v" }, "u", "u", { desc = "Undo" })
-            vim.keymap.set({ "n", "v" }, "U", "<C-r>", { desc = "Redo" })
-          end
-
-          vim.api.nvim_create_user_command("Colemak", function()
-            colemak()
-          end, {
-            desc = "Set keybindings to Colemak",
-          })
-
-          vim.api.nvim_create_user_command("Qwerty", function()
-            require("keyboard").qwerty()
-          end, {
-            desc = "Set keybindings to Qwerty",
-          })
-          
-          local vscode = require("vscode-neovim")
-          vim.notify = vscode.notify
-
-          local call = function(method)
-            return function()
-              vscode.call(method)
-            end
-          end 
-
-          colemak()
-
-          -- Generated bindings
-          vim.keymap.set("n", "<CR>", call("editor.action.smartSelect.expand"))
-          vim.keymap.set("v", "<CR>", call("editor.action.smartSelect.expand"))
-
-          vim.keymap.set("n", "<S-CR>", call("editor.action.smartSelect.shrink"))
-          vim.keymap.set("v", "<S-CR>", call("editor.action.smartSelect.shrink"))
-
-          vim.keymap.set("n", "gd", call("editor.action.revealDefinition"))
-          vim.keymap.set("v", "gd", call("editor.action.revealDefinition"))
-
-          vim.keymap.set("n", "gg", call("cursorTop"))
-          vim.keymap.set("v", "gg", call("cursorTop"))
-
-          vim.keymap.set("n", "gi", call("editor.action.goToImplementation"))
-          vim.keymap.set("v", "gi", call("editor.action.goToImplementation"))
-
-          vim.keymap.set("n", "gr", call("editor.action.goToReferences"))
-          vim.keymap.set("v", "gr", call("editor.action.goToReferences"))
-
-          vim.keymap.set("n", "zac", call("editor.foldAll"))
-          vim.keymap.set("v", "zac", call("editor.foldAll"))
-
-          vim.keymap.set("n", "zae", call("editor.unfoldAll"))
-          vim.keymap.set("v", "zae", call("editor.unfoldAll"))
-
-          vim.keymap.set("n", "zo", call("editor.foldAllExcept"))
-          vim.keymap.set("v", "zo", call("editor.foldAllExcept"))
-
-          vim.keymap.set("n", "zc", call("editor.fold"))
-          vim.keymap.set("v", "zc", call("editor.fold"))
-
-          vim.keymap.set("n", "ze", call("editor.unfold"))
-          vim.keymap.set("v", "ze", call("editor.unfold"))   
-      EOF
-    '';
-  };
-  /*  
-    wayland.windowManager.hyprland.enable = true;
-    wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        "$mod, RETURN, exec, foot"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
-    };  
-   */
-
   programs.foot.enable = true;
   programs.foot.settings = {
     main = {
       font = "Noto Sans Mono:size=11";
     };
   };
-
-  programs.wofi.enable = true;
-  programs.wofi.settings = {
-    columns = 2;
-  };
-  programs.wofi.style =
-    ''
-    * {
-    font-family: monospace;
-    }
-
-    window {
-    background-color: #7c818c;
-    }
-    ''
-  ;
 
   home.stateVersion = "23.11";
 
--- sway.nix
+++ sway.nix
@@ -158,4 +158,21 @@
       };
     };
   };
+
+  programs.wofi.enable = true;
+  programs.wofi.settings = {
+    columns = 2;
+  };
+  programs.wofi.style =
+    ''
+      * {
+      font-family: monospace;
+      }
+
+      window {
+      background-color: #7c818c;
+      }
+    ''
+  ;
+
 }
