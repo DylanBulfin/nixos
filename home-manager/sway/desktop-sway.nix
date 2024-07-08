d89527fa89deba64845035f27e9f7bece247b6b6
Dylan Bulfin <dylanbulfin@gmail.com>
2024-07-08 18:24:02 -0400

    Adding a bunch of desktop sway updates

--- home-manager/sway/desktop-sway.nix
+++ home-manager/sway/desktop-sway.nix
@@ -122,38 +122,11 @@
       menu = "${pkgs.wofi}/bin/wofi --show drun";
       window.hideEdgeBorders = "both";
       window.titlebar = false;
-      window.commands = [
-        {
-          command = "move to workspace 2; workspace 2";
-          criteria = {
-            app_id = "foot";
-          };
-        }
-        {
-          command = "move to workspace 3; workspace 3";
-          criteria = {
-            app_id = "floorp";
-          };
-        }
-        {
-          command = "move to workspace 5; workspace 5";
-          criteria = {
-            class = "Code";
-          };
-        }
-        {
-          command = "move to workspace 1; workspace 1";
-          criteria = {
-            class = "ArmCord";
-          };
-        }
-        {
-          command = "move to workspace 5; workspace 5";
-          criteria = {
-            app_id = "thunar";
-          };
-        }
-      ];
+      assigns = {
+        "2" = [{ app_id = "^foot$"; }];
+        "5" = [{ app_id = "^Code$"; }];
+        "4" = [{ app_id = "^thunar$"; }];
+      };
       input = {
         "*" = {
           scroll_factor = "0.50";
@@ -161,9 +134,29 @@
       };
       output = {
         DP-1 = {
-          
+          mode = "2560x1440";
+          position = "2560,0";
+        };
+        DP-2 = {
+          mode = "2560x1440";
+          position = "5120,0";
+        };
+        DP-3 = {
+          mode = "2560x1440";
+          position = "0,0";
         };
       };
+      workspaceOutputAssign = [
+        { output = "DP-1"; workspace = "2"; }
+        { output = "DP-1"; workspace = "5"; }
+        { output = "DP-1"; workspace = "8"; }
+        { output = "DP-2"; workspace = "3"; }
+        { output = "DP-2"; workspace = "6"; }
+        { output = "DP-2"; workspace = "9"; }
+        { output = "DP-3"; workspace = "1"; }
+        { output = "DP-3"; workspace = "4"; }
+        { output = "DP-3"; workspace = "7"; }
+      ];
     };
   };
 
