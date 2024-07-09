832b1ee3cddaa25e3a8118f723104b18c146c59f
Dylan Bulfin <dylanbulfin@gmail.com>
2024-07-09 18:22:42 -0400

    Combine most waybar config

--- home-manager/sway/sway.nix
+++ home-manager/sway/sway.nix
@@ -4,6 +4,77 @@
   imports = [
     ./${device}-sway.nix
   ];
+  
+  programs.waybar.enable = true;
+  home.file."wallpaper.png".source = ./wallpaper.png;
+
+  programs.waybar.style = ./style.css;
+
+  programs.waybar.settings = {
+    mainBar = {
+      layer = "top";
+      position = "top";
+      height = 30;
+      modules-left = [ "sway/workspaces" "sway/mode" ];
+      modules-center = [ "sway/window" ];
+      modules-right = [ "pulseaudio" "network" "cpu" "memory" "temperature" "backlight" ]
+        ++ (if device == "laptop" then [ "battery" ] else [ ])
+        ++ [ "clock" ];
+      "clock" = {
+        # "timezone"= "America/New_York",
+        "tooltip-format" = "<big>{=%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        "format-alt" = "{=%Y-%m-%d}";
+      };
+      "cpu" = {
+        "format" = "{usage}% ";
+        "tooltip" = false;
+      };
+      "memory" = {
+        "format" = "{}% ";
+      };
+      "temperature" = {
+        # "thermal-zone"= 2;
+        # "hwmon-path"= "/sys/class/hwmon/hwmon2/temp1_input";
+        "critical-threshold" = 80;
+        # "format-critical"= "{temperatureC}°C {icon}";
+        "format" = "{temperatureC}°C {icon}";
+        "format-icons" = [ "" "" "" ];
+      };
+      "backlight" = {
+        # "device"= "acpi_video1";
+        "format" = "{percent}% {icon}";
+        "format-icons" = [ "" "" "" "" "" "" "" "" "" ];
+      };
+      "network" = {
+        # "interface"= "wlp2*", // (Optional) To force the use of this interface
+        "format-wifi" = "{essid} ({signalStrength}%) ";
+        "format-ethernet" = "{ipaddr}/{cidr} ";
+        "tooltip-format" = "{ifname} via {gwaddr} ";
+        "format-linked" = "{ifname} (No IP) ";
+        "format-disconnected" = "Disconnected ⚠";
+        "format-alt" = "{ifname}= {ipaddr}/{cidr}";
+      };
+      "pulseaudio" = {
+        # "scroll-step"= 1, // %, can be a float
+        "format" = "{volume}% {icon}    {format_source}";
+        "format-bluetooth" = "{volume}% {icon}     {format_source}";
+        "format-bluetooth-muted" = " {icon}     {format_source}";
+        "format-muted" = "     {format_source}";
+        "format-source" = "{volume}%     ";
+        "format-source-muted" = "";
+        "format-icons" = {
+          "headphone" = "";
+          "hands-free" = "";
+          "headset" = "";
+          "phone" = "";
+          "portable" = "";
+          "car" = "";
+          "default" = [ "" "" "" ];
+        };
+        "on-click" = "pavucontrol";
+      };
+    };
+  };
 
   wayland.windowManager.sway = {
     enable = true;
