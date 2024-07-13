9ceec5d2f55c246d39600f0dbd7a2862bded55b7
Dylan Bulfin <dylanbulfin@gmail.com>
2024-07-12 21:10:44 -0400

    Adding firefox settings to supress crash resume

--- home-manager/modules/firefox/firefox.nix
+++ home-manager/modules/firefox/firefox.nix
@@ -5,13 +5,16 @@
 
   programs.firefox.profiles.dylan = {
 
-    userChrome= ''
+    userChrome = ''
       #TabsToolbar {
           display: none !important;
       }
     '';
 
     settings = {
+      "browser.sessionstore.max_resumed_crashes" = 0;
+      "browser.sessionstore.resume_from_crash" = false;
+
       "extensions.activeThemeID" = "firefox-compact-light@mozilla.org";
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
     };
