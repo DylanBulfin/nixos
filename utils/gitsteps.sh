2d296d57628d84aa8121a80deaa92f13bb9c4cc0
Dylan Bulfin <dylanbulfin@gmail.com>
2024-11-29 09:31:49 -0500

    Fix scripts up

--- utils/gitsteps.sh
+++ utils/gitsteps.sh
@@ -2,14 +2,18 @@
 # #!/usr/bin/env bash
 ## The second should work if the first doesn't
 
-nixosdir="/etc/nixos"
+# dylan-laptop or dylan-desktop
+device = $1
+derivation = dylan-$device
 
 # Need the config to be a git repository, this is why we re-download
-# cd $nixosdir
-# rm -rf *
-#
-# git clone https://github.com/DylanBulfin/nixos .
+cd "/etc/nixos"
+rm -rf *
 
-echo $@
-echo testing
-echo $#
+git clone https://github.com/DylanBulfin/nixos .
+
+nixos-generate-config
+rm configuration.nix
+mv hardware-configuration.nix nixos/hardware/$device-hardware.nix
+
+sudo nixos-rebuild boot --flake .#$derivation
--- utils/install.sh
+++ utils/install.sh
@@ -4,12 +4,21 @@
 
 if ! (sudo -v); then
   1>&2 echo "Unable to get sudo auth, aborting"
+  exit 1
 fi
 
-curl -L --output /home/dylan/Downloads/nixos.zip https://github.com/DylanBulfin/nixos/archive/refs/heads/main.zip
-cd /home/dylan/Downloads
+if [ "$#" != 1 ]; then 
+  1>&2 echo "Wrong number of arguments"
+  exit 1
+fi
 
-nix shell --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#unzip --command sh -c "unzip nixos.zip"
+if [ "$1" != "laptop" ] && [ "$1" != "desktop" ]; then
+  1>&2 echo "Unsupported argument"
+  exit 1
+fi
 
-chmod +x /home/dylan/Downloads/nixos-main/utils/gitsteps.sh
-nix shell --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#git --command /home/dylan/Downloads/nixos-main/utils/gitsteps.sh ArgTest
+cd /home/dylan
+curl -L --output ./gitsteps.sh https://github.com/DylanBulfin/nixos/blob/main/utils/gitshell.sh
+
+chmod +x /home/dylan/gitsteps.sh
+nix shell --extra-experimental-features nix-command --extra-experimental-features flakes nixpkgs#git --command /home/dylan/gitsteps.sh $1
